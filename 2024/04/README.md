e1e09fafebb8e72eb289d365b706e5efc1f6125e
imcalvin <kelvinwong0519@gmail.com>
2025-06-23 22:38:51 -0700

    2024-04 day 4 finish

--- 2024/04/README.md
+++ 2024/04/README.md
@@ -6,3 +6,15 @@
 - can appear left to right, right to left, top to bottom, bottom to top, diagonal top left to bottom right, diagonal top right to bottom left, diagonal bottom left to top right, diagonal bottom right to top left
 
 - I can take the input in and put it as a 2d array, and then iterate through it. whenever I see an `X`, I can do a search. This is pretty slow, and exploits that the word `XMAS` contains no duplicates of `X`
+
+### Part 2
+
+- this is diabolical 
+- we want to find `MAS` in a cross pattern. there's only 4 possibilities.
+
+1. m: in the top left and top right
+2. m: in the top left and bot right
+3. m: in the bot left and bot right
+4. m: in the top right and bot right
+
+- we can probably use the same thing, where we look for `a` and then search for the square from there, doing each search exhaustively.
--- 2024/04/main.go
+++ 2024/04/main.go
@@ -14,7 +14,7 @@ func check(err error) {
 
 // searches the input array at position (x, y) in 8 different ways for the word "SEARCH_TERM"
 // left to right, right to left, top to bottom, bottom to top, diagonal top left to bottom right, diagonal top right to bottom left, diagonal bottom left to top right, diagonal bottom right to top left
-func search(input [][]rune, x int, y int, searchTermStr string) int {
+func search_part_1(input [][]rune, x int, y int, searchTermStr string) int {
 	counter := 0 // max possible matches per search
 	const WORD_LEN = 4
 	searchTerm := []rune(searchTermStr)
@@ -59,9 +59,43 @@ func search(input [][]rune, x int, y int, searchTermStr string) int {
 	return counter
 }
 
+func search_part_2(input [][]rune, x int, y int) int {
+	n, m := len(input), len(input[0])
+
+	// search a square around, this time the edges are easier
+	if x-1 < 0 || x+1 >= n || y-1 < 0 || y+1 >= m {
+		return 0
+	}
+
+	// if 'M' is in one corner, then we expect 'S' to be in the other one
+	corners := [][]int{
+		{-1, -1}, // top left
+		{-1, 1},  // top right
+	}
+
+	numMatches := 0
+
+	for _, corner := range corners {
+		k, l := corner[0], corner[1]
+
+		char := input[x+k][y+l]
+		opposite := input[x+k*-1][y+l*-1]
+
+		if (char == 'M' && opposite == 'S') || (char == 'S' && opposite == 'M') {
+			numMatches++
+		}
+	}
+
+	if numMatches == 2 { // we formed a box!
+		return 1
+	}
+
+	return 0
+}
+
 func main() {
 	fmt.Println("Hello, World!")
-	const SEARCH_TERM = "XMAS"
+	// const SEARCH_TERM = "XMAS"
 
 	file, err := os.Open("./input")
 	check(err)
@@ -85,8 +119,8 @@ func main() {
 	// 2d array for input
 	for i := range input {
 		for j := range input[i] {
-			if input[i][j] == 'X' {
-				counter = counter + search(input, i, j, SEARCH_TERM)
+			if input[i][j] == 'A' {
+				counter = counter + search_part_2(input, i, j)
 			}
 		}
 	}
