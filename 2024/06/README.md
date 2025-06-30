486032ba432dce487a2d7cf58e0cca5dc43244d4
im-calvin <kelvinwong0519@gmail.com>
2025-06-30 16:08:44 -0700

    2024-06 part 2 fail

--- 2024/06/README.md
+++ 2024/06/README.md
@@ -5,3 +5,20 @@
 - if you see a `#` change direction
 - use a `while(true)` loop?
 - at the end grab all the `X`'s
+
+### Part 2
+- making a loop is the same as making sure the guard crosses a new `X` at some point
+- we should still make that `X` map, and then every time there's a box then put an `O` there
+  - a 'box' would mean that the guard will create a box
+  - this means that we *shouldn't* make the `X` map, but calculate this inline.
+- as we make our `X` map, if we were to cross over an `X` at any point, then that's a location we can add a possible box
+- this won't work because we need to account for the orientation of the guard whenever he reaches an `X`. we could make an `equals()` func to check for orientation AND location
+  - each square could have multiple orientations...
+
+- new idea, for each square we travel, we try to place an obstacle down in front of us. you can only place one down if when you place one down you match the orientation and location.. (and also an obstacle wasn't placed there to begin with..)
+
+- there's 2 types of obstacles that we can place
+1. we've crossed where we've been through before and it forms a new box
+2. we haven't visited it before but it'll stil form a box... 
+  - we might be able to deal with this by looping through the visitedMap and then seeing if any of them form boxes afterwards
+  - can't we condense #1 and #2 into one by doing this? 
--- 2024/06/main.go
+++ 2024/06/main.go
@@ -21,6 +21,12 @@ const (
 	LEFT
 )
 
+type Visited struct {
+	Direction Direction
+	X         int
+	Y         int
+}
+
 func (d Direction) Next() Direction {
 	return Direction((int(d) + 1) % 4)
 }
@@ -85,20 +91,31 @@ func main() {
 	}
 
 	n, m := len(maze), len(maze[0])
-
+	// keep track of orientation & location that we traveled to
+	traveledNodes := make(map[Visited]bool)
 	direction := UP // start in the up dir
+
 	// while we are in the bounds
 	for x >= 0 && x < n && y >= 0 && y < m {
 		// travel to current square
 		curr := maze[x][y]
+		visited := Visited{
+			direction,
+			x,
+			y,
+		}
+
 		switch curr {
+		// golang switch cases won't execute 'X' if '#' executes
 		case '#':
 			// need to backtrack to get off the blockade
 			x, y = backtrack(x, y, direction)
+			// if we backtrack we will always land on an 'X'
 			direction = direction.Next() // this will move to the right because of the enum
 			continue
 		default:
 			maze[x][y] = 'X'
+			traveledNodes[visited] = false // visit the current node with the current direction
 		}
 
 		x, y = forward(x, y, direction)
@@ -114,6 +131,31 @@ func main() {
 		}
 	}
 
+	numObstructions := 0
+	for visited := range traveledNodes {
+		// we only want to place obstacles where there haven't already been obstacles!
+
+		// if "false" then the node has been visited but an obstacle hasn't already placed in that spot
+		// visited is where I currently am, and obstacle placements happens in front of me. need to check that there is no obstacle in front of me
+		x, y := forward(visited.X, visited.Y, visited.Direction)
+		if x >= 0 && x < n && y >= 0 && y < m && maze[x][y] == '#' {
+			continue
+		}
+
+		futureX, futureY := forward(visited.X, visited.Y, visited.Direction.Next())
+		futureVisited := Visited{
+			visited.Direction.Next(), // the obstacle would reorient, direction.Next() will not mutate `direction`
+			futureX,
+			futureY,
+		}
+
+		if elem, ok := traveledNodes[futureVisited]; ok && !elem {
+			numObstructions++
+			traveledNodes[visited] = true
+		}
+	}
+
 	fmt.Println(numVisited)
+	fmt.Println(numObstructions)
 
 }
